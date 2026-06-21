87b973bd1671aaf04e728e1145b7ba4f4d5e4b17
Nicolas Lestrez <nicolas.lestrez@gmail.com>
2026-06-21 13:56:43 +0200

    Ajout sitemap.xml

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://portfolio-nicolas-sigma.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
